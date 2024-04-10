d249fc839b4c36d173ea5e975df1ce92fbf7f4b4
BrayanOel <159608236+BrayanOel@users.noreply.github.com>
2024-04-09 23:32:17 -0400

    Add files via upload
    Inicio

---
+++ MonsterCard.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GwentPro;
+
+public class MonsterCards
+{
+	public int Power { get; set; }
+	public string Name { get; set; }
+	public string Faction { get; set; }
+	public TipeMonster Tipe{ get; set; }
+
+	public void createCard(int power, string name, string faction , TipeMonster tipe)
+	{
+		Power = power;
+		Name = name;
+		Faction = faction;
+		Tipe = tipe;
+	}
+	
+	public bool isTipe(TipeMonster tipe)
+	{
+	    return Tipe == tipe;
+	}
+}
+
+public enum TipeMonster
+{
+    Warrior,
+	Archer,
+	Support,
+}
+
+/*MonsterCards myCard = new MonsterCards(TipeMonster.Warrior);
+
+if (mycard.isTipe(TipeMonster.Warrior))
+{
+    //codigo si myCard es guerrero
+}*/
